680ac088ac547a64d6954f0801d531a98babe402
Paul Walter <paul@recordsone.com>
2020-03-08 13:11:19 -0400

    Updated the script file to hand putting up and down the images. (used to be two separate scripts)

--- down.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Shut down and destroy containers
-bin/moodle-docker-compose down
--- script.sh
+++ script.sh
@@ -18,17 +18,24 @@ dir_moodle_docker_bin="$dir_bin/moodle-docker/bin"
 repo_moodle="https://github.com/moodle/moodle.git"
 repo_moodle_docker="https://github.com/moodlehq/moodle-docker"
 
-# STEP 1: INIT
+# STEP 1: INIT EXPORTS
 
 	export MOODLE_DOCKER_WWWROOT=$dir_moodle
 	
 	# Choose a db server (Currently supported: pgsql, mariadb, mysql, mssql, oracle)
 	export MOODLE_DOCKER_DB=pgsql
-
+	
+	# Hopefully the person running this script has docker-compose installed, we 
+	# need to export it into the script environment if so the Moodle-Docker project
+	# can use it (we are calling it's scripts indirectly)
+	export docker-compose
+	
+# STEP 1a: INIT repos
+	
 	# Check to see if the bin folder exists, if it does, then assume this project is already configured. 
 	if [ ! -d $dir_bin ]; then
 	  	
-	  	echo "bin folder doesn't exist, creating..."
+	  	echo "* bin folder doesn't exist, creating..."
 	  	mkdir -p $dir_bin;
 	  
 	  	git clone $repo_moodle_docker $dir_moodle_docker
@@ -40,24 +47,38 @@ repo_moodle_docker="https://github.com/moodlehq/moodle-docker"
 	fi
 	
 	if [ -d $dir_bin ]; then
-		echo "Bin folder exists: skipping initialization"
+		echo "* Bin folder exists: no need to re-clone the projects."
 	fi
 
 # STEP 2: RUN
 
-	# Hopefully the person running this script has docker-compose installed, we need to export it into the script environment if so.
-	export docker-compose
+	echo "* test: $2, $1s"
+	case "$1" in
 	
-	# Start up containers
-	/bin/bash "$dir_moodle_docker_bin/moodle-docker-compose" "up" "-d"
+	"up" | "1")
+	    # Start up containers
+		/bin/bash "$dir_moodle_docker_bin/moodle-docker-compose" "up" "-d"
+		
+		# Wait for DB to come up (important for oracle/mssql)
+		/bin/bash "$dir_moodle_docker_bin/moodle-docker-wait-for-db"
 	
-	# Wait for DB to come up (important for oracle/mssql)
-	/bin/bash "$dir_moodle_docker_bin/moodle-docker-wait-for-db"
+		# Work with the containers (see below)
+		# [..]
+		
+		echo "DB is up";
+	    ;;
+    "down" | "2")
+	    # Shut down and destroy containers
+		/bin/bash "$dir_moodle_docker_bin/moodle-docker-compose" "down"
+	    ;;
+	*)
+		echo " "
+		echo "Available Commands: "
+		echo "  up (1)"
+	    echo "  down (2)"
+	esac
+
 	
-	# Work with the containers (see below)
-	# [..]
 	
-	echo "DB is up";
 	
-	# Shut down and destroy containers
-	/bin/bash "$dir_moodle_docker_bin/moodle-docker-compose" "down"
+	
