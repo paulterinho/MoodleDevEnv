9c674c1644e44609a4e6744f4415efa227bb11fe
Paul Walter <paul@recordsone.com>
2020-03-13 14:26:53 -0400

    Auto configuring XDebug

--- script.sh
+++ script.sh
@@ -14,6 +14,9 @@ dir_moodle="$dir_bin/moodle"
 dir_moodle_docker="$dir_bin/moodle-docker"
 dir_moodle_docker_bin="$dir_bin/moodle-docker/bin"
 
+# Container Directory Vars
+dir_php_ini = "/usr/local/etc/php/"
+
 # Repo Vars
 repo_moodle="https://github.com/moodle/moodle.git"
 repo_moodle_docker="https://github.com/moodlehq/moodle-docker"
@@ -30,7 +33,7 @@ repo_moodle_docker="https://github.com/moodlehq/moodle-docker"
 	# can use it (we are calling it's scripts indirectly)
 	export docker-compose
 	
-# STEP 1a: INIT repos
+# STEP 1b: INIT repos
 	
 	# Check to see if the bin folder exists, if it does, then assume this project is already configured. 
 	if [ ! -d $dir_bin ]; then
@@ -44,15 +47,19 @@ repo_moodle_docker="https://github.com/moodlehq/moodle-docker"
 	  
 		# Ensure customized config.php for the Docker containers is in place
 		cp "$dir_moodle_docker/config.docker-template.php" $MOODLE_DOCKER_WWWROOT/config.php
+		
+		# STEP 1a: Open port 9000 in the config file so we can install XDebugger later
+		echo "      - 9000:9000" >> ./bin/moodle-docker/webserver.port.yml
+		
 	fi
 	
 	if [ -d $dir_bin ]; then
 		echo "* Bin folder exists: no need to re-clone the projects."
 	fi
+	
 
 # STEP 2: RUN
 
-	echo "* test: $2, $1s"
 	case "$1" in
 	
 	"up" | "1")
@@ -62,8 +69,24 @@ repo_moodle_docker="https://github.com/moodlehq/moodle-docker"
 		# Wait for DB to come up (important for oracle/mssql)
 		/bin/bash "$dir_moodle_docker_bin/moodle-docker-wait-for-db"
 	
-		# Work with the containers (see below)
-		# [..]
+			# Work with the containers (see below)
+			# 	Log into the docker container and 
+			docker exec -it moodle-docker_webserver_1 /bin/bash
+			
+			pecl install xdebug
+			
+			# Copy the dev php.ini to just be a plain php.ini (weird it's not already like this)
+			cp /usr/local/etc/php/php.ini-development /usr/local/etc/php/php.ini
+			
+			# Append configuration updates to the php.ini file
+			echo ' zend_extension="/usr/local/lib/php/extensions/no-debug-non-zts-20170718/xdebug.so"' >> /usr/local/etc/php/php.ini
+			# xdebug.profiler_enable=1;
+			# xdebug.profiler_output_dir=/php-tmp
+			# xdebug.profiler_output_name-cachegrind.out.%R.%t
+
+			# sudo service apache2 restart (this should kick out out of the docker shell)
+			/etc/init.d/apache2 restart
+			
 		
 		echo "DB is up";
 	    ;;
@@ -71,11 +94,24 @@ repo_moodle_docker="https://github.com/moodlehq/moodle-docker"
 	    # Shut down and destroy containers
 		/bin/bash "$dir_moodle_docker_bin/moodle-docker-compose" "down"
 	    ;;
+	    
+   "init behat tests" | "3")
+	    # Initialize behat environment
+		bin/moodle-docker-compose exec webserver php admin/tool/behat/cli/init.php
+		;; 
+		
+	"init behat tests" | "4")
+	    # Run behat tests
+		bin/moodle-docker-compose exec -u www-data webserver php admin/tool/behat/cli/run.php --tags=@auth_manual
+		;; 
+
 	*)
 		echo " "
 		echo "Available Commands: "
 		echo "  up (1)"
 	    echo "  down (2)"
+	    echo "  init behat tests (3)"
+	    echo "  run behat tests (4)"
 	esac
 
 	
